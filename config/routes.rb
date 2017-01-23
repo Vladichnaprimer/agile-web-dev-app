1a882d97b72455097f1452ff016bede408f098e3
vladich <vladichnaprimer@gmail.com>
2017-01-23 12:36:57 +0200

    Add localization feature

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,20 @@ Rails.application.routes.draw do
       post 'login' => :create
       delete 'logout' => :destroy
   end
-  
+
   resources :users
   resources :orders
   resources :line_items
   resources :carts
 
-  root to: 'store#index', as: 'store'
+  scope '(:locale)' do
+      resources :orders
+      resources :line_items
+      resources :carts
+      root 'store#index', as: 'store', via: :all
+  end
+
+  # root to: 'store#index', as: 'store'
 
   resources :products do
       get :who_bought, on: :member
